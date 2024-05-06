3df60a8f82c516cd75db59348c3b1905fc0f57d2
manueligno78 <manuelpintaldi@gmail.com>
2024-05-06 22:24:14 +0200

    New sb theme (#9)
    * gherkinDocumentToString support description of scenarios
    
    * new sb theme
    
    * New settings page
    
    * Refactoring
    
    * New tagCloud component
    
    * Favicon
    
    * Scenario Outline reports number of examples present
    
    * Add card to sceanario layout
    
    * Filter by clicking on tag cloud
    
    * Filter by clicking on tag cloud
    
    * Add a tag feature
    
    * fix export
    
    * Fix exporter
    
    * Steps examples and dataTables
    
    * Delete and Modify all tag occurrencie
    
    * git status wip

---
+++ gherkinUtils.js
@@ -0,0 +1,82 @@
+
+function gherkinDocumentToString(gherkinDocument) {
+    //console.log('gherkinDocument:', JSON.stringify(gherkinDocument, null, 2));
+    let gherkinText = '';
+    // Add the feature tags, if any
+    if (gherkinDocument.feature.tags && gherkinDocument.feature.tags.length > 0) {
+        const tags = gherkinDocument.feature.tags.map(tag => tag.name).join(' ');
+        gherkinText += `${tags}\n`;
+    }
+    // Add the feature title
+    gherkinText += `Feature: ${gherkinDocument.feature.name}\n`;
+    // Add the feature description, if any
+    if (gherkinDocument.feature.description) {
+        gherkinText += `${gherkinDocument.feature.description}\n`;
+    }
+    // Add each scenario or background
+    gherkinDocument.feature.children.forEach((child, index, array) => {
+        gherkinText += '\n';
+        if (child.background) { // TODO: bug: title is not displayed
+            gherkinText += `\tBackground: ${child.background.name}\n`;
+            // Add each step of the background
+            child.background.steps.forEach(step => {
+                gherkinText += `\t\t${step.keyword} ${step.text}\n`;
+            });
+            // Add a blank line after the background
+            gherkinText += '\n';
+        } else if (child.scenario) {
+            // Add the scenario tags, if any
+            if (child.scenario.tags && child.scenario.tags.length > 0) {
+                const tags = child.scenario.tags.map(tag => tag.name).join(' ');
+                gherkinText += `\t${tags}\n`;
+            }
+            // Check if the scenario is an outline
+            if (child.scenario.examples && child.scenario.examples.length > 0) {
+                gherkinText += `\tScenario Outline: ${child.scenario.name}\n`;
+            } else {
+                gherkinText += `\tScenario: ${child.scenario.name}\n`;
+            }
+            // Add the scenario description, if any
+            if (child.scenario.description) {
+                gherkinText += `${child.scenario.description}\n`;
+            }
+            // Add each step of the scenario
+            child.scenario.steps.forEach((step, stepIndex) => {
+                gherkinText += `\t\t${step.keyword}${step.text}`;
+                // Check if the step has a datatable
+                if (step.dataTable) {
+                    step.dataTable.rows.forEach(row => {
+                        gherkinText += '\n\t\t\t| ' + row.cells.map(cell => cell.value).join(' | ') + ' |';
+                    });
+                }
+                if (stepIndex < child.scenario.steps.length) {
+                    gherkinText += '\n';
+                }
+            });
+            // Add the Examples, if any
+            if (child.scenario.examples && child.scenario.examples.length > 0) {
+                child.scenario.examples.forEach(example => {
+                    gherkinText += `\n\t\tExamples:\n`;
+                    // Add the table header
+                    const header = example.tableHeader.cells.map(cell => cell.value).join(' \t| ');
+                    gherkinText += `\t\t\t| ${header} |\n`;
+                    // Add the table rows
+                    example.tableBody.forEach((row, rowIndex) => {
+                        const rowText = row.cells.map(cell => cell.value).join(' \t| ');
+                        gherkinText += `\t\t\t| ${rowText} |`;
+                        if (rowIndex < example.tableBody.length) {
+                            gherkinText += '\n';
+                        }
+                    });
+                });
+            }
+            // // Add a blank line after each scenario, except the last one
+            // if (index < array.length - 1) {
+            //     gherkinText += '\n';
+            // }
+        }
+    });
+    return gherkinText;
+}
+
+module.exports = gherkinDocumentToString;
--- index.js
+++ index.js
@@ -4,68 +4,36 @@ const fs = require('fs');
 const path = require('path');
 const Gherkin = require('@cucumber/gherkin');
 const Messages = require('@cucumber/messages');
-const { exec } = require('child_process');
+const { exec, execSync } = require('child_process');
 const WebSocket = require('ws');
 const http = require('http');
+const gherkinDocumentToString = require('./gherkinUtils');
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
+let config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-module.exports = server;
+module.exports.server = server;
+module.exports.getFiles = getFiles;
+module.exports.getScenarios = getScenarios;
 
-function getScenarios(filePath) {
-  try {
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    const uuidFn = Messages.IdGenerator.uuid();
-    const builder = new Gherkin.AstBuilder(uuidFn);
-    const matcher = new Gherkin.GherkinClassicTokenMatcher();
-    const parser = new Gherkin.Parser(builder, matcher);
-    const gherkinDocument = parser.parse(fileContent);
-    const pickles = Gherkin.compile(gherkinDocument, filePath, uuidFn);
-    const scenarios = pickles.map(pickle => {
-      const isOutline = gherkinDocument.feature.children.some(child => 
-        child.scenario && child.scenario.examples && child.scenario.examples.length > 0 && child.scenario.name === pickle.name
-      );
-      return {
-        name: pickle.name,
-        tags: pickle.tags.map(tag => ({
-          name: tag.name,
-          color: hashCode(tag.name)
-        })),
-        isOutline: isOutline
-      };
-    });
-    const allTags = pickles.flatMap(pickle => pickle.tags.map(tag => ({
-      name: tag.name,
-      color: hashCode(tag.name)
-    })));
-    const uniqueTags = [...new Set(allTags.map(tag => tag.name))];
-    const featureTitle = gherkinDocument.feature.name;
-    return {
-      featureTitle: featureTitle,
-      scenarioCount: scenarios.length,
-      scenarios: scenarios,
-      tags: uniqueTags
-    };
-  } catch (error) {
-    console.error(`Error getting scenarios: ${error}`);
-    return null;
-  }
-}
+let featureFilesCopy = [];
 
 function getFiles(dirPath, arrayOfFiles = []) {
   const files = fs.readdirSync(dirPath);
-  const foldersToExclude = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8')).folderToExclude;
-  const excludePatterns = foldersToExclude.split(',').map(folder => 
-    new RegExp('^' + folder.trim().replace(/\*/g, '.*') + '$')
-  );
+  const foldersToExclude = config.folderToExclude;
+  let excludePatterns = [];
+  if (foldersToExclude) {
+    excludePatterns = foldersToExclude.split(',').map(folder =>
+      new RegExp('^' + folder.trim().replace(/\*/g, '.*') + '$')
+    );
+  }
   files.forEach(file => {
     const fullPath = path.join(dirPath, "/", file);
     const isExcluded = excludePatterns.some(pattern => pattern.test(fullPath));
@@ -80,6 +48,7 @@ function getFiles(dirPath, arrayOfFiles = []) {
         arrayOfFiles.push({
           name: file,
           path: fullPath,
+          relativePath: fullPath.replace(config.directoryPath, ''),
           ...scenarioData
         });
       }
@@ -88,45 +57,88 @@ function getFiles(dirPath, arrayOfFiles = []) {
   return arrayOfFiles;
 }
 
+function getScenarios(filePath) {
+  try {
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const uuidFn = Messages.IdGenerator.uuid();
+    const builder = new Gherkin.AstBuilder(uuidFn);
+    const matcher = new Gherkin.GherkinClassicTokenMatcher();
+    const parser = new Gherkin.Parser(builder, matcher);
+    const gherkinDocument = parser.parse(fileContent);
 
-function hashCode(str) {
-  var hash = 0;
-  for (var i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    const featureId = uuidFn();
+    const tags = [];
+
+    gherkinDocument.feature.children.forEach(child => {
+      if (child.scenario) {
+        child.scenario.tags.forEach(tag => {
+          tags.push({
+            tag: tag.name,
+            scenario: child.scenario.name,
+            featureId: featureId
+          });
+        });
+        child.scenario.isOutline = child.scenario.keyword.includes('Outline');
+        if (child.scenario.isOutline && child.scenario.examples) {
+          child.scenario.numberOfExamples = child.scenario.examples.reduce((count, example) => {
+            return count + (example.tableBody ? example.tableBody.length : 0);
+          }, 0);
+        }
+        // Count the steps in the scenario
+        child.scenario.numberOfSteps = child.scenario.steps ? child.scenario.steps.length : 0;
+      }
+    });
+
+    return {
+      featureId: featureId,
+      tags: tags,
+      ...gherkinDocument
+    };
+  } catch (error) {
+    console.error(`Error getting scenarios: ${error}`);
+    return null;
   }
-  var color = '#';
-  for (var i = 0; i < 3; i++) {
-    var value = (hash >> (i * 8)) & 0xFF;
-    color += ('00' + value.toString(16)).substr(-2);
-  }
-  return color;
 }
 
+/// SERVER and WEBSOCKETS
 function notifyClients(message) {
   wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-          client.send(message);
-      }
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
   });
 }
 
+function gitStatus() {
+  // Execute a shell command
+  // git status on config.directoryPath
+  const gitStatusCommand = 'git status --porcelain';
+  const gitStatusOutput = execSync(gitStatusCommand, { cwd: config.directoryPath });
+  return gitStatusOutput.toString();
+}
+
 app.get('/', (req, res) => {
-  const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
+  config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
   const directoryPath = config.directoryPath;
   if (!directoryPath) {
     res.redirect('/settings');
   } else {
-    let featureFiles = getFiles(directoryPath);
-    res.render('table', { featureFiles: featureFiles, runCommand: !!config.testCommand });
+    if (featureFilesCopy.length === 0) {
+      let featureFiles = getFiles(directoryPath);
+      featureFilesCopy = JSON.parse(JSON.stringify(featureFiles));
+    }
+    res.render('index', { configuration: config, featureFiles: featureFilesCopy, runCommand: !!config.testCommand });
   }
 });
 
 app.get('/settings', (req, res) => {
-  const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
+  config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
   res.render('settings', {
     directoryPath: config.directoryPath,
     testCommand: config.testCommand,
-    folderToExclude: config.folderToExclude
+    folderToExclude: config.folderToExclude,
+    outputFolder: config.outputFolder,
+    keepFolderStructure: config.keepFolderStructure
   });
 });
 
@@ -134,38 +146,181 @@ app.post('/save-settings', (req, res) => {
   const newDirectoryPath = req.body.directoryPath;
   const newTestCommand = req.body.testCommand;
   const newFolderToExclude = req.body.folderToExclude;
+  const newOutputFolder = req.body.outputFolder;
+  const newKeepFolderStructure = req.body.keepFolderStructure === 'on';
   const newConfig = {
-      directoryPath: newDirectoryPath,
-      testCommand: newTestCommand,
-      folderToExclude: newFolderToExclude
+    directoryPath: newDirectoryPath,
+    testCommand: newTestCommand,
+    folderToExclude: newFolderToExclude,
+    outputFolder: newOutputFolder,
+    keepFolderStructure: newKeepFolderStructure
   };
-  fs.writeFileSync(path.join(__dirname, 'config.json'), JSON.stringify(newConfig, null, 2), 'utf-8');
-  res.redirect('/settings');
+  config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
+  if (JSON.stringify(config) !== JSON.stringify(newConfig)) {
+    fs.writeFileSync(path.join(__dirname, 'config.json'), JSON.stringify(newConfig, null, 2), 'utf-8');
+    reset();
+  }
+  res.redirect('/');
 });
-
 
 wss.on('connection', ws => {
   ws.on('message', message => {
-    console.log(`Received message => ${message}`);
-    
     const data = JSON.parse(message);
     if (data.action === 'run-tests') {
       const tags = data.tags;
-      const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
       const testCommand = config.testCommand.replace('@yourTag', tags);
-      
+
       notifyClients('tests started');
       notifyClients('executing: ' + testCommand);
       notifyClients('debug: ' + tags);
-      
+
       exec(testCommand, (error, stdout, stderr) => {
         // ... gestione dell'output come prima ...
       });
     }
+    if (data.action === 'updateFeature') {
+      let featureFile = featureFilesCopy.find(file => file.featureId === data.featureId);
+      if (featureFile) {
+        setNestedProperty(featureFile, data.field, data.newValue);
+      }
+      notifyClients(JSON.stringify({ action: 'featureUpdated', featureId: data.featureId, field: data.field, newValue: data.newValue }));
+    }
+    if (data.action === 'removeTag') {
+      let featureFile = featureFilesCopy.find(file => file.featureId === data.featureId);
+      if (featureFile) {
+        let scenario = featureFile.feature.children.find(child => child.scenario && child.scenario.id === data.scenarioId);
+        if (scenario) {
+          let tagIndex = scenario.scenario.tags.findIndex(tag => tag.name === data.tag);
+          if (tagIndex > -1) {
+            scenario.scenario.tags.splice(tagIndex, 1);
+            let scenarioIndex = featureFile.feature.children.findIndex(child => child.scenario && child.scenario.id === data.scenarioId);
+            featureFile.feature.children[scenarioIndex] = scenario;
+            let featureIndex = featureFilesCopy.findIndex(file => file.featureId === data.featureId);
+            featureFilesCopy[featureIndex] = featureFile;
+            notifyClients(JSON.stringify({ action: 'featureUpdated', featureId: data.featureId, field: 'tags', newValue: scenario.scenario.tags }));
+          }
+        }
+      }
+    }
+    if (data.action === 'addTag') {
+      let featureFile = featureFilesCopy.find(file => file.featureId === data.featureId);
+      if (featureFile) {
+        let scenario = featureFile.feature.children.find(child => child.scenario && child.scenario.id === data.scenarioId);
+        if (scenario) {
+          let tagExists = scenario.scenario.tags.some(tag => tag.name === data.tag);
+          if (!tagExists) {
+            scenario.scenario.tags.push({ name: data.tag });
+            let scenarioIndex = featureFile.feature.children.findIndex(child => child.scenario && child.scenario.id === data.scenarioId);
+            featureFile.feature.children[scenarioIndex] = scenario;
+            let featureIndex = featureFilesCopy.findIndex(file => file.featureId === data.featureId);
+            featureFilesCopy[featureIndex] = featureFile;
+            notifyClients(JSON.stringify({ action: 'featureUpdated', featureId: data.featureId, field: 'tags', newValue: scenario.scenario.tags }));
+          }
+        }
+      }
+    }
+    if (data.action === 'saveOnDisk') {
+      const outputFolder = config.outputFolder;
+      const keepFolderStructure = config.keepFolderStructure;
+      const directoryPath = config.directoryPath;
+      featureFilesCopy.forEach(featureFile => {
+        const gherkinText = gherkinDocumentToString(featureFile);
+        let outputUrl = featureFile.path.replace(directoryPath, outputFolder);
+        if (keepFolderStructure) {
+          outputUrl = outputUrl.replace(/\\/g, '\\\\');
+        }
+        ensureDirectoryExistence(outputUrl);
+        fs.writeFileSync(outputUrl, gherkinText);
+      });
+    }
+    // Delete all occurency of a tag from all the scenarios and features
+    if (data.action === 'deleteAllOccurencyOfTag') {
+      featureFilesCopy.forEach(featureFile => {
+        featureFile.feature.children.forEach(child => {
+          if (child.scenario) {
+            let tagIndex = child.scenario.tags.findIndex(tag => tag.name === data.tag);
+            if (tagIndex > -1) {
+              child.scenario.tags.splice(tagIndex, 1);
+            }
+          }
+        });
+      });
+      notifyClients(JSON.stringify({ action: 'featureUpdated', tag: data.tag }));
+    }
+    // Update all occurency of a tag from all the scenarios and features only if the tag exists and newTag does not exist else notify the client that the newTag already exists
+    if (data.action === 'updateAllOccurencyOfTag') {
+      let tagExists = false;
+      let newTagExists = false;
+      featureFilesCopy.forEach(featureFile => {
+        featureFile.feature.children.forEach(child => {
+          if (child.scenario) {
+            let tagIndex = child.scenario.tags.findIndex(tag => tag.name === data.tag);
+            if (tagIndex > -1) {
+              tagExists = true;
+              let newTagIndex = child.scenario.tags.findIndex(tag => tag.name === data.newTag);
+              if (newTagIndex > -1) {
+                newTagExists = true;
+              } else {
+                child.scenario.tags[tagIndex].name = data.newTag;
+              }
+            }
+          }
+        });
+      });
+      if (tagExists && !newTagExists) {
+        notifyClients(JSON.stringify({ action: 'featureUpdated', tag: data.tag, newTag: data.newTag }));
+      } else if (newTagExists) {
+        notifyClients(JSON.stringify({ action: 'error', message: data.newTag + ' already exists' }));
+      }
+    }
+    if (data.action === 'reset') {
+      reset();
+    }
+    if (data.action === 'gitStatus') {
+      notifyClients(JSON.stringify({ action: 'gitStatus', message: gitStatus() }));
+    }
   });
 });
 
+function getNestedProperty(obj, path) {
+  return path.split('.').reduce((prev, curr) => {
+    return prev ? prev[curr] : null
+  }, obj || self)
+}
+
+function reset() {
+  config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
+  const directoryPath = config.directoryPath;
+  let featureFiles = getFiles(directoryPath);
+  //let featureFiles = getFiles(directoryPath);
+  featureFilesCopy = JSON.parse(JSON.stringify(featureFiles));
+  notifyClients(JSON.stringify({ action: 'reset' }));
+}
+
+function setNestedProperty(obj, path, value) {
+  const pathParts = path.split(/[\.\[\]]/).filter(part => part);
+  const lastPart = pathParts.pop();
+
+  const target = pathParts.reduce((prev, curr) => {
+    return prev ? prev[curr] : null
+  }, obj || self);
+
+  if (target && lastPart) {
+    target[lastPart] = value;
+  } else {
+    console.error('Error setting nested property', path, 'on', obj);
+  }
+}
+
+function ensureDirectoryExistence(filePath) {
+  const dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
 
 server.listen(3000, () => {
-  console.log('App listening on http://localhost:3000');
+  console.log('CukeHub listening on http://localhost:3000');
 });
